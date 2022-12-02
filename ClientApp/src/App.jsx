fdffd1e2ab3dd1bf01e65d7c6c43b98e71839085
Harrison <harrisonsmall12@gmail.com>
2022-12-02 09:24:44 -0700

    placeholder navbar and adding mui dependancies

--- ClientApp/src/App.jsx
+++ ClientApp/src/App.jsx
@@ -1,36 +1,27 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import Box from '@mui/material/Box'
+import Container from '@mui/material/Container'
+import Navbar from './components/Navbar/Navbar'
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-        <p>
-          test api here
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    <Container maxWidth="sm">
+      <Navbar />
+      <Box 
+        sx={{
+          width: 500,
+          height: 500,
+          backgroundColor: 'primary.dark',
+          '&:hover': {
+            backgroundColor: 'secondary.dark'
+          },
+        }}
+      />
+    </Container>
+    
   )
 }
 
