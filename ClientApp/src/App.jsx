c5b8167c852174b45dd5f4637fc16a7722574c31
Harrison <harrisonsmall12@gmail.com>
2022-12-02 19:58:15 -0700

    follow up to last commit, starting work on frontend queries

--- ClientApp/src/App.jsx
+++ ClientApp/src/App.jsx
@@ -1,19 +1,25 @@
 import { useState } from 'react';
-import Box from '@mui/material/Box';
-import Stack from '@mui/material/Stack';
-import Container from '@mui/material/Container';
+import {
+  useQuery,
+  useQueryClient,
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query';
+import { Stack } from '@mui/material';
 import Navbar from './components/Navbar/Navbar';
 import PokeInfo from './components/PokeInfo/PokeInfo';
 
+const queryClient = new QueryClient();
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <Stack direction="column" spacing="0">
-      <Navbar />
-      <PokeInfo />
-    </Stack>
+    <QueryClientProvider client={queryClient}>
+      <Stack direction="column" spacing="0">
+        <Navbar />
+        <PokeInfo />
+      </Stack>
+    </QueryClientProvider>
   )
 }
 
--- ClientApp/src/components/MovesList/Move.jsx
+++ ClientApp/src/components/MovesList/Move.jsx
@@ -1,12 +1,22 @@
 import React from 'react';
 import {
-    Box
-    Typography
-    Link
+    Box,
+    Typography,
+    Link,
 } from '@mui/material'
+import { useQuery } from '@tanstack/react-query';
 
 // query to get move data: name, type, phys/sp, acc, pwr
+async function fetchMove(name) {
+    try {
+        const moveData = await fetch(`https://pokeapi.co/api/v2/move/${name}/`);
+        return await moveData.json();
+    } catch (err) {
+        console.log(err)
+    }
+    
+}
 
 export default function Move({ name, url }) {
-    
+    const {isLoading, isError, data, error } = useQuery('move', fetchMove)
 }
